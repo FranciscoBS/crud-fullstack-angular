3d4e060173521a0bb1910f9ebe7a0153d374e70b
FranciscoBSGN3 <jbarradas@gn3rh.com>
2025-10-20 18:28:40 -0600

    No es nada

--- crud-fullstack-angular/src/main/java/com/francisco/crud_fullstack_angular/exception/ResourceNotFoundException.java
+++ crud-fullstack-angular/src/main/java/com/francisco/crud_fullstack_angular/exception/ResourceNotFoundException.java
@@ -3,10 +3,8 @@ package com.francisco.crud_fullstack_angular.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-    private String message;
     public ResourceNotFoundException(String message) {
         super(message);
-        this.message = message;
     }   
 
 }
